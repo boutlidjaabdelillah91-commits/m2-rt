9aabcd2433b5077e2cfb373564a6b9686957638a
boutlidjaabdelillah91-commits <boutlidjaabdelillah91@gmail.com>
2025-12-15 19:32:40 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import numpy as np
+import pickle
+
+# Charger le modèle et le scaler
+model = pickle.load(open("model.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
+
+st.set_page_config(page_title="House Price Prediction")
+
+st.title("🏠 House Price Prediction")
+st.write("Enter the house features")
+
+MedInc = st.number_input("Median Income", min_value=0.0)
+HouseAge = st.number_input("House Age", min_value=0.0)
+AveRooms = st.number_input("Average Rooms", min_value=0.0)
+AveBedrms = st.number_input("Average Bedrooms", min_value=0.0)
+Population = st.number_input("Population", min_value=0.0)
+AveOccup = st.number_input("Average Occupancy", min_value=0.0)
+Latitude = st.number_input("Latitude")
+Longitude = st.number_input("Longitude")
+
+if st.button("Predict Price"):
+    input_data = np.array([[MedInc, HouseAge, AveRooms, AveBedrms,
+                            Population, AveOccup, Latitude, Longitude]])
+
+    input_scaled = scaler.transform(input_data)
+    prediction = model.predict(input_scaled)
+
+    st.success(f"Estimated House Price: {prediction[0]:.2f}")
